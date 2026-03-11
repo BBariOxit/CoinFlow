df24eaca2bc1fed874990d2dd892cecb755c8b06
phanthaibao <phanbao2648@gmail.com>
2026-03-11 22:56:24 +0700

    implement navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
@@ -9,9 +10,10 @@ const Header = () => {
   const pathname = usePathname()
   return (
     <header>
-      <div className="main-container inner">
-        <Link href="/">
-          <Image src="/public/logo.svg" alt="CoinFlow logo" width={132} height={40} />
+      <div className="container inner">
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="CoinFlow logo" width={30} height={30} />
+          <span className="text-white font-semibold text-lg">CoinFlow</span>
         </Link>
 
         <nav>
@@ -19,8 +21,13 @@ const Header = () => {
             'is-active': pathname === '/',
             'is-home': true 
           })}>Home</Link>
+
           <p>Search model</p>
-          <Link href='/coins'>All coins</Link>
+
+          <Link href='/coins' className={cn('nav-link', {
+            'is-active': pathname === '/coins',
+            'is-home': true 
+          })}>All coins</Link>
         </nav>
       </div>
     </header>
