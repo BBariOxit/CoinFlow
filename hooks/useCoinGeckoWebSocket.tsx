1e60e5516cd761e0368ba5d86cb892e5abbb4e72
phanthaibao <phanbao2648@gmail.com>
2026-03-27 14:29:06 +0700

    custom useOkxWebSocket

---
+++ hooks/useCoinGeckoWebSocket.tsx
@@ -0,0 +1,115 @@
+import { channel } from "diagnostics_channel";
+import { useEffect, useRef, useState } from "react";
+const WS_BASE = "";
+
+export const useCoinGeckoWebSocket = ({
+  coinId,
+  poolId,
+  liveInterval,
+}: UseCoinGeckoWebSocketProps): UseCoinGeckoWebSocketReturn => {
+  const wsRef = useRef<WebSocket | null>(null);
+  const subscribed = useRef<Set<string>>(new Set());
+
+  const [price, setPrice] = useState<ExtendedPriceData | null>(null);
+  const [trades, setTrades] = useState<Trade[]>([]);
+  const [ohlcv, setOhlcv] = useState<OHLCData | null>(null);
+
+  const [isWsReady, setIsWsReady] = useState(false);
+
+  useEffect(() => {
+    const ws = new WebSocket(WS_BASE);
+    wsRef.current = ws;
+
+    const send = (payload: Record<string, unknown>) => ws.send(JSON.stringify(payload));
+
+    const handleMessage = (event: MessageEvent) => {
+      const msg: WebSocketMessage = JSON.parse(event.data);
+
+      if (msg.type === "ping") {
+        send({ type: "pong" });
+        return;
+      }
+
+      if (msg.type === "confirm-subscription") {
+        const { channel } = JSON.parse(msg?.identifier ?? "");
+        subscribed.current.add(channel);
+      }
+
+      if (msg.c === "C1") {
+        setPrice({
+          usd: msg.p ?? 0,
+          coin: msg.i,
+          price: msg.p,
+          change24h: msg.pp,
+          marketCap: msg.m,
+          volume24h: msg.v,
+          timestamp: msg.t,
+        });
+      }
+
+      if (msg.c === "G2") {
+        const newTrade: Trade = {
+          price: msg.pu,
+          value: msg.vo,
+          timestamp: msg.t ?? 0,
+          type: msg.ty,
+          amount: msg.to,
+        };
+        setTrades((prev) => [newTrade, ...prev].slice(0, 7));
+      }
+
+      if (msg.ch === "G3") {
+        const timestamp = msg.t ?? 0;
+        const candle: OHLCData = [
+          timestamp,
+          Number(msg.o ?? 0),
+          Number(msg.h ?? 0),
+          Number(msg.l ?? 0),
+          Number(msg.c ?? 0),
+        ];
+        setOhlcv(candle);
+      }
+    };
+
+    ws.onopen = () => setIsWsReady(true);
+
+    ws.onmessage = handleMessage;
+    ws.onclose = () => setIsWsReady(false);
+
+    return () => ws.close();
+  }, []);
+
+  useEffect(() => {
+    if (!isWsReady) return;
+
+    const ws = wsRef.current;
+    if (!ws) return;
+    const send = (payload: Record<string, unknown>) => ws.send(JSON.stringify(payload));
+
+    const unsubscribeAll = () => {
+      subscribed.current.forEach((channel) => {
+        send({
+          command: "unsubscribe",
+          identifier: JSON.stringify({ channel }),
+        });
+      });
+      subscribed.current.clear();
+    };
+
+    const subscribe = (channel: string, data?: Record<string, unknown>) => {
+      if (subscribed.current.has(channel)) return;
+      send({
+        command: "subscribe",
+        identifier: JSON.stringify({ channel }),
+      });
+
+      if (data) {
+        send({
+          command: "message",
+          identifier: JSON.stringify({ channel }),
+          data: JSON.stringify(data),
+        });
+      }
+    };
+  }, [coinId, poolId, isWsReady, liveInterval]);
+};
