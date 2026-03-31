e1336db69a12dcec6c5adc5e00a371330771ab49
phanthaibao <phanbao2648@gmail.com>
2026-03-31 09:55:31 +0700

    implement coinHeader component

--- components/LiveDataWrapper.tsx
+++ components/LiveDataWrapper.tsx
@@ -6,11 +6,12 @@ import { Separator } from "./ui/separator";
 import { formatCurrency, timeAgo } from "@/lib/utils";
 import DataTable from "./DataTable";
 import { useState } from "react";
+import CoinHeader from "./CoinHeader";
 
 const LiveDataWrapper = ({ children, coinId, poolId, coin, coinOHLCData }: LiveDataProps) => {
   const [liveInterval, setLiveInterval] = useState<"1s" | "1m">("1s");
 
-  const { trades, ohlcv } = useOkxWebSocket({ coinId, poolId, liveInterval });
+  const { trades, ohlcv, price } = useOkxWebSocket({ coinId, poolId, liveInterval });
 
   const tradeColumns: DataTableColumn<Trade>[] = [
     {
@@ -56,7 +57,16 @@ const LiveDataWrapper = ({ children, coinId, poolId, coin, coinOHLCData }: LiveD
 
   return (
     <section id="live-data-wrapper">
-      <p>Coin header</p>
+      <CoinHeader
+        name={coin.name}
+        image={coin.image.large}
+        livePrice={price?.usd ?? coin.market_data.current_price.usd}
+        livePriceChangePercentage24h={
+          price?.change24h ?? coin.market_data.price_change_percentage_24h_in_currency.usd
+        }
+        priceChangePercentage30d={coin.market_data.price_change_percentage_30d_in_currency.usd}
+        priceChange24h={coin.market_data.price_change_24h_in_currency.usd}
+      />
       <Separator className="divider" />
       <div className="trend">
         <CandlestickChart
